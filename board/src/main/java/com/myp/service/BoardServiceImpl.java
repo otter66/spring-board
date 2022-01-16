89f7d59a3f22d134f9eb11d514264cd3f5d807be
suyeon6 <jmssk11@naver.com>
2022-01-16 20:01:01 +0900

    FIX BoardDAOImpl.java

--- board/src/main/java/com/myp/service/BoardServiceImpl.java
+++ board/src/main/java/com/myp/service/BoardServiceImpl.java
@@ -2,38 +2,41 @@ package com.myp.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.myp.domain.BoardVO;
+import com.myp.persistence.BoardDAO;
 
 @Repository
 public class BoardServiceImpl implements BoardService {
 	
+	@Autowired
+	private BoardDAO boardDAO;
+	
 	@Override
 	public void regist(BoardVO board) throws Exception {
-		// TODO Auto-generated method stub
+		boardDAO.create(board);
 	}
 
 	@Override
 	public BoardVO read(Integer bno) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		return boardDAO.read(bno);
 	}
 
 	@Override
 	public void modify(BoardVO board) throws Exception {
-		// TODO Auto-generated method stub
+		boardDAO.update(board);
 	}
 
 	@Override
 	public void remove(Integer bno) throws Exception {
-		// TODO Auto-generated method stub
+		boardDAO.delete(bno);
 	}
 
 	@Override
 	public List<BoardVO> listAll() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		return boardDAO.listAll();
 	}
 
 }
